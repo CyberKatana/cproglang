c9bb4be9665ae63341c6985b4eb930ecce437dce
CyberKatana <puptsevaa@gmail.com>
2024-12-23 22:20:03 +0400

    Solved exercises 1.4

---
+++ chapter1/ex_1.4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	float fahr,celsius;
+	float lower, upper, step;
+	float COEF;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	COEF = 5.0 / 9.0;//Cache constat coefficietn of formela C=(5/9)*(F-32)
+
+	printf("Farenheit Celsius\n");//Add heading to table
+
+	celsius = lower;
+	while (celsius <= upper)
+	{
+		fahr = (celsius / COEF) + 32.0;
+		printf("%6.0f, %8.1f\n", fahr, celsius);
+		celsius = celsius + step;
+	}
+}
